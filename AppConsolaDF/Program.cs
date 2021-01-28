ab03170cfac4d247b8b6f2ace2bb98a5705230d0
Kenneth Gaitán <kenneth.gaitan-ext@dominion-global.com>
2021-01-28 17:40:26 -0600

    Add project files.

---
+++ AppConsolaDF/Program.cs
@@ -0,0 +1,84 @@
+﻿using DataAccess;
+using DataAccess.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AppConsolaDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trabajos trabajo = new Trabajos {
+                Titulo = "Trabajo con registros sincrónicos",
+                Ubicacion = "Ciudad de méxico",
+                Salario = 1870,
+                Descripcion = "Se necesita de un programador que sepa manejar metodos sincrónicos.",
+                TipoContratoId = 2,
+                CategoriaTrabajoId = 12,
+                FechaRegistro = DateTime.Now,
+                FechaModificacion = DateTime.Now,
+                Estado = true
+            };
+
+            var trabajoAsincronico = new TrabajoRepositorio().AgregarTrabajoAsync(trabajo);
+
+            Console.WriteLine("3. este código se ejecuta despues de obtener un trabajo.");
+
+            Console.WriteLine("4. este código se ejecuta a continuación.");
+
+            Console.WriteLine("5. este código se ejecuta al final.");
+
+            Console.WriteLine($"Estado: {trabajoAsincronico.Result.OK}, Mensaje: {trabajoAsincronico.Result.Mensaje}");
+
+
+            //var agregarTrabajo = new TrabajoRepositorio().AgregarTrabajo(trabajo);
+
+            //Console.WriteLine("3. este código se ejecuta despues de obtener un trabajo.");
+
+            //Console.WriteLine("4. este código se ejecuta a continuación.");
+
+            //Console.WriteLine("5. este código se ejecuta al final.");
+
+            //Console.WriteLine($"ID: {trabajo.Id}, TÍTULO: {trabajo.Titulo}");
+
+            //Console.WriteLine($"ESTADO: {agregarTrabajo.OK}, MENSAJE: {agregarTrabajo.Mensaje}");
+
+
+            //var trabajo = new TrabajoRepositorio().ObtenerTrabajo(1);
+
+            //Console.WriteLine("3. este código se ejecuta despues de obtener un trabajo.");
+
+            //Console.WriteLine("4. este código se ejecuta a continuación.");
+
+            //Console.WriteLine("5. este código se ejecuta al final.");
+
+            //Console.WriteLine($"ID: {trabajo.Id}, TÍTULO: {trabajo.Titulo}");
+
+            //ejecutar
+            //var trabajo = new TrabajoRepositorio().ObtenerTrabajoAsync(1);
+
+            //Console.WriteLine("3. este código se ejecuta despues de obtener un trabajo.");
+
+            //Console.WriteLine("4. este código se ejecuta a continuación.");
+
+            //Console.WriteLine("5. este código se ejecuta al final.");
+
+            //Console.WriteLine($"ID: {trabajo.Result.Id}, TÍTULO: {trabajo.Result.Titulo}");
+
+
+            Console.ReadKey();
+        }
+
+
+        
+    }
+}
---
+++ DataAccess/Repositorio/TrabajoRepositorio.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositorio
+{
+    public class TrabajoRepositorio
+    {
+        public Trabajos ObtenerTrabajo(int trabajoId)
+        {
+            Trabajos trabajo = null;
+
+            using (var db = new TrabajosConexion())
+            {
+                //db.Database.Log = Console.WriteLine;
+
+                Console.WriteLine("1. obteniendo trabajo...");
+
+                trabajo = db.Trabajos.FirstOrDefault(x => x.Id == trabajoId);
+
+                Console.WriteLine("2. trabajo obtenido.");
+            }
+
+            return trabajo;
+        }
+
+        //metodo asincronico
+        public async Task<Trabajos> ObtenerTrabajoAsync(int trabajoId)
+        {
+            Trabajos trabajo = null;
+
+            using (var db = new TrabajosConexion())
+            {
+                Console.WriteLine("1. obteniendo trabajo asincrónico...");
+
+                trabajo = await (db.Trabajos.FirstOrDefaultAsync(x => x.Id == trabajoId));
+
+                Console.WriteLine("2. trabajo asincrónico obtenido.");
+            }
+
+            return trabajo;
+        }
+    
+        public MensajeRespuesta AgregarTrabajo(Trabajos trabajo)
+        {
+            MensajeRespuesta respuesta = new MensajeRespuesta();
+
+            try
+            {
+                //codigo
+                using (var db = new TrabajosConexion())
+                {
+                    db.Entry(trabajo).State = EntityState.Added;
+
+                    Console.WriteLine("1. Agregando nuevo trabajo.");
+
+                    db.SaveChanges();
+
+                    Console.WriteLine("2. Trabajo agregado correctamente.");
+                }
+
+            } catch(Exception ex)
+            {
+                respuesta = new MensajeRespuesta(ex.Message);
+            }
+
+            return respuesta;
+        }
+
+        //metodo asincrónico
+        public async Task<MensajeRespuesta> AgregarTrabajoAsync(Trabajos trabajo)
+        {
+            MensajeRespuesta respuesta = new MensajeRespuesta();
+
+            try
+            {
+                //codigo
+                using (var db = new TrabajosConexion())
+                {
+
+                    db.Entry(trabajo).State = EntityState.Added;
+
+                    Console.WriteLine("1. Agregando nuevo trabajo asincrónico.");
+
+                    await (db.SaveChangesAsync());
+
+                    Console.WriteLine("2. Trabajo asincrónico agregado correctamente.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                respuesta = new MensajeRespuesta($"{ex.Message} - {ex.InnerException}");
+            }
+
+            return respuesta;
+        }
+    }
+
+    public class MensajeRespuesta
+    {
+        public bool OK { get; set; }
+        public string Mensaje { get; set; }
+
+        public MensajeRespuesta()
+        {
+            OK = true;
+            Mensaje = "transacción realizada correctamente";
+        }
+
+        public MensajeRespuesta(string mensaje)
+        {
+            OK = false;
+            Mensaje = string.Format($"ERROR: {mensaje}");
+        }
+    }
+}
